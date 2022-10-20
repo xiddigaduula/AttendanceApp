841d8ed4ff77ad92f33dfb3daeb17351a3608839
khalid ali <xiddig33@gmail.com>
2022-10-20 14:49:01 +0300

    5th commit
    Signed-off-by: khalid ali <xiddig33@gmail.com>

--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -1,76 +1,74 @@
-import React from 'react';
+import React from "react";
 import {
   SafeAreaView,
   View,
   Text,
   TextInput,
   TouchableOpacity,
-} from 'react-native';
+} from "react-native";
 
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
+import CustomButton from "../Components/CustomButton";
+import InputField from "../Components/InputField";
 
-import CustomButton from '../Components/CustomButton';
-import InputField from '../Components/InputField';
-
-const LoginScreen = ({navigation}) => {
+const LoginScreen = ({ navigation }) => {
   return (
-    <SafeAreaView style={{flex: 1, justifyContent: 'center'}}>
-      <View style={{paddingHorizontal: 25}}>
-        
-
+    <SafeAreaView style={{ flex: 1, justifyContent: "center" }}>
+      <View style={{ paddingHorizontal: 25 }}>
         <Text
           style={{
-            fontFamily: 'Roboto-Medium',
+            fontFamily: "Roboto-Medium",
             fontSize: 28,
-            fontWeight: '500',
-            color: '#333',
+            fontWeight: "500",
+            color: "#333",
             marginBottom: 30,
-          }}>
+          }}
+        >
           Login
         </Text>
 
         <InputField
-          label={'Email ID'}
+          label={"Email ID"}
           icon={
             <MaterialIcons
-            name="alternate-email"
-            size={20}
-            color="#666"
-            style={{marginRight: 5}}
-          />
+              name="alternate-email"
+              size={20}
+              color="#666"
+              style={{ marginRight: 5 }}
+            />
           }
           keyboardType="email-address"
         />
 
-<InputField
-          label={'Password'}
+        <InputField
+          label={"Password"}
           icon={
             <Ionicons
-            name="ios-lock-closed-outline"
-            size={20}
-            color="#666"
-            style={{marginRight: 5}}
-          />
+              name="ios-lock-closed-outline"
+              size={20}
+              color="#666"
+              style={{ marginRight: 5 }}
+            />
           }
           inputType="password"
           fieldButtonLabel={"Forgot?"}
           fieldButtonFunction={() => {}}
         />
-        
+
         <CustomButton label={"Login"} onPress={() => {}} />
 
-        
         <View
           style={{
-            flexDirection: 'row',
-            justifyContent: 'center',
+            flexDirection: "row",
+            justifyContent: "center",
             marginBottom: 30,
-          }}>
+          }}
+        >
           <Text>Create New Account</Text>
-          <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-            <Text style={{color: '#AD40AF', fontWeight: '700'}}> SignUp</Text>
+          <TouchableOpacity onPress={() => navigation.navigate("Login")}>
+            <Text style={{ color: "#AD40AF", fontWeight: "700" }}> SignUp</Text>
           </TouchableOpacity>
         </View>
       </View>
